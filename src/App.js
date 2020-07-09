6e9d299353c4009a28fbf56a7bc41492f37ab10b
Alyssa Nycum <alyssa.nycum@gmail.com>
2020-07-09 16:18:38 -0500

    created components for login and register

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom'
 import NavBar from '../src/components/navbar/Navbar';
 import ApplicationViews from './ApplicationViews';
 import './App.css';
 
 const App = () => {
+  const isAuthenticated = () => sessionStorage.getItem("betadata-token") !== null;
+
+  const [hasUser, setHasUser] = useState(isAuthenticated());
+
+  const setUser = () => {
+    setHasUser(isAuthenticated());
+  };
+
+  const clearUser = () => {
+    sessionStorage.clear()
+    setHasUser(isAuthenticated());
+  }
+
   return (
     <Router>
-      <NavBar />
-      <ApplicationViews />
+      <NavBar hasUser={hasUser} clearUser={clearUser} />
+      <ApplicationViews hasUser={hasUser} setUser={setUser} />
     </Router>
   );
 };
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import { withRouter, Link } from 'react-router-dom';
 
-const NavBar = () => {
+const NavBar = (props) => {
+    const handleLogout = () => {
+        props.clearUser()
+        props.history.push("/");
+    }
+
     return (
         <header>
             <h1>BetaData</h1>
+            <nav>
+                <ul>
+                    {props.hasUser ? 
+                        <li>
+                            <Link to="" onClick={handleLogout}>Logout</Link>
+                        </li>
+                    :
+                        <>
+                            <li>
+                                <Link to="/login">Login</Link>
+                            </li>
+                            <li>
+                                <Link to="/register">Register</Link>
+                            </li>
+                        </>
+                    }
+                </ul>
+            </nav>
         </header>
     )
 };
